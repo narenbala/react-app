f2131b5420372045f43f7ba7790d035936bbfda9
narenbala <narenbala91@gmail.com>
2022-06-09 11:38:14 +0530

    my commit

--- my-react-website/src/App.js
+++ my-react-website/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Clock from './clock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         <h1>Hello react</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='outer'>
+   <div className='clock'>
+     <div className='hand-second'>
+       <div className='second'></div>
+     </div>
+     <div className='hand-minute'>
+       <div className='minute'></div>
+     </div>
+     <div className='hand-hours'>
+       <div className='hours'></div>
+     </div>
+   </div>
+   
+   </div>
+   
   );
+  
 }
 
 export default App;
